bd92c039f91cd6834f6dccaa2f41ec34a3de44a3
maksimpopov94icloud-spec <maksimpopov94icloud@gmail.com>
2026-02-27 11:53:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -44,6 +44,9 @@ document.querySelectorAll('.menu-btn').forEach(button => {
         } else if (section === 'feedback') {
             tg.openTelegramLink('https://t.me/exportNO_bot');
         } else {
+        } else if (section === 'news') {
+            tg.openTelegramLink('https://t.me/ВАШ_НОВОСТНОЙ_БОТ'); // замените на username вашего нового бота
+        }
             tg.showAlert(`Раздел "${button.textContent}" будет доступен в ближайшее время`);
         }
     });
